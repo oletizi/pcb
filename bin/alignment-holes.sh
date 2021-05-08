21a50274f9ef512d59a58aaa1a84521fe29994d4
Orion Letizi <oletizi@mac.com>
2021-05-08 12:36:10 -0700

    mods to alignment-hole script

--- bin/alignment-holes.sh
+++ bin/alignment-holes.sh
@@ -1,21 +1,24 @@
 #!/bin/sh
 
+depth="-6"
+feed="120"
+
 cmd="pcb2gcode \
   --metric=1 \
   --metricoutput=1 \
   --zsafe=2 \
   --zchange=15 \
   --cutter-diameter=1 \
-  --cut-feed=30 \
+  --cut-feed=${feed} \
   --cut-speed=10000 \
   --cut-infeed=0.075 \
-  --zcut=-0.075 \
+  --zcut=-0.2 \
   --drill $1 \
   --milldrill-diameter=1 \
   --min-milldrill-hole-diameter=0 \
-  --zdrill=-5 \
-  --zmilldrill=5 \
-  --drill-feed=10 \
+  --zdrill=${depth} \
+  --zmilldrill=${depth} \
+  --drill-feed=${feed} \
   --drill-speed=10000 \
   --milldrill-output=alignment-holes.nc"
 
